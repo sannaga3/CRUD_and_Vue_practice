d182e0b0c10c16a2d1a1ec550429debc192d66ce
sannaga3 <n.tetsuya02@gmail.com>
2021-09-06 19:25:14 +0900

    Userモデル及びコントローラ作成

--- app/javascript/task.vue
+++ app/javascript/task.vue
@@ -37,8 +37,8 @@ const client = axios.create({
   }
 })
 client.defaults.headers.common = {
-    'X-Requested-With': 'XMLHttpRequest',
-    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+  'X-Requested-With': 'XMLHttpRequest',
+  'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
 }
 export default {
   data: function () {
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, except: :index
   resources :tasks, only: %i[ index create update destroy ]
   root to: "memos#index"
   resources :memos, only: %i[ index create update destroy ]
